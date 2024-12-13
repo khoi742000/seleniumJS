a3c40968710b847ae4389e0c146d9929da7e6c70
maidangkhoi <74171770+maidangkhoi@users.noreply.github.com>
2024-12-13 21:15:31 +0700

    121221

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,25 +1,40 @@
-const { it } = require('node:test');
-const LoginPage = require('../pages/login.page');
-const driverChrome = require('../utils/driver');
+const { LoginPage } = require('../pages/login.page.js');
 
-describe('Login Tests', () => {
-    let driver;
+const { assert, equal } = require('assert');
+const { By } = require('selenium-webdriver');
+
+describe('Login Tests', function() {
+    this.timeout(10000); // Tăng timeout lên 10 giây
     let login
 
-    before(async() => {
-        driver = new driverChrome();
-        login = new LoginPage();
 
+    before(async function() {
+        login = new LoginPage()
+
+
+    })
+    beforeEach(async function() {
+        await login.openDriver()
+        await login.openURL()
     });
 
-    after(async() => {
-        if (driver) {
-            await driver.quit();
+    afterEach(async function() {
+        if (login && login.driver) {
+            // Kiểm tra xem driver có hợp lệ không trước khi gọi quit()
+            try {
+                await login.quit(); // Đảm bảo gọi quit() sau mỗi test
+            } catch (error) {
+                console.error("Error quitting driver:", error);
+            }
         }
     });
 
-    it('login ne', () => {
-        login.LoginPage()
+
+    it('login ne', async function() {
+        await login.loginSuccess()
+        const actully = await login.fin
+        await equal(actully, "Gợi ý để bán hàng thuận lợi hơn", "text không đúng")
+        console.log("pass")
 
     })
 
